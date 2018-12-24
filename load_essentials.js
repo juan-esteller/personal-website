7af841d5722fcf7a4852ee4f52a62ba8dd82b732
Juan Esteller <juanvicenteesteller@gmail.com>
2018-12-24 03:23:12 -0500

    Do header.

---
+++ load_essentials.js
@@ -0,0 +1,8 @@
+document.getElementById("myNav").innerHTML =
+    "<ul class='header' id = 'navlinks'>" +
+    "<li class='header'><a href='/index.html' class='header'>Home</a></li>" +
+    "<li class='header'><a href='/about.html' class='header'>About</a></li>" +
+    "<li class='header'><a href='/professional.html' class='header'>Professional</a></li>" +
+    "<li class='header'><a href='/personal.html' class='header'>Personal</a></li>" +
+    "<li class='header'><a href='/website.html' class='header'>Website</a></li>" +
+    "</ul>";
